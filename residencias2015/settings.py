a6649ffdc9f89b8e4700527bae45a70c2ef8c2b2
marta <marta@marta-VPCEB17FL.(none)>
2015-09-30 12:54:03 -0300

    cambio database

--- residencias2015/settings.py
+++ residencias2015/settings.py
@@ -1,5 +1,5 @@
 # Django settings for residencias2015 project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -132,7 +132,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
